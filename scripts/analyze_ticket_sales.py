177036aca077a25cab5b2f694517a3319452f61a
Chow Li <chowli1103@gmail.com>
2020-11-15 01:19:54 -0800

    fixed indention

--- scripts/analyze_ticket_sales.py
+++ scripts/analyze_ticket_sales.py
@@ -13,7 +13,7 @@ def query_popular_tickets(connection):
             event_ranks AS (
                 SELECT event_name, 
                        num_events,
-					   RANK () OVER (ORDER BY num_events) AS event_rank
+                       RANK () OVER (ORDER BY num_events) AS event_rank
                 FROM events
             )
 
